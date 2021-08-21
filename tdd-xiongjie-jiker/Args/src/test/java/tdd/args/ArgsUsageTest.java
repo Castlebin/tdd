a08a1cc8d6e3bf25c77221ce15eba788fc9fe9f8
Castlebin <Castlebin@gmail.com>
2021-08-21 11:37:26 +0800

    初始化 Args 项目

---
+++ tdd-xiongjie-jiker/Args/src/test/java/tdd/args/ArgsUsageTest.java
@@ -0,0 +1,19 @@
+package tdd.args;
+
+import org.junit.jupiter.api.Test;
+
+public class ArgsUsageTest {
+
+    @Test
+    public void testArgs() {
+        String argsText = "-l -p 8080 -d /usr/logs";
+
+        // 1. 把命令行字符串拆分成main函数可用的字符串数组
+
+        // 2. 字符串形式的Schema解析成对象
+
+        // 3. 根据Schema和参数字符串数组获取参数值
+
+    }
+
+}
