eeae3e1744281174307514b4b19b70253e0fcb2b
Castlebin <Castlebin@gmail.com>
2021-08-21 13:25:09 +0800

    Args 第一版（不支持参数值为负数，因为解析命令行时，直接用 - 进行分割的）

--- tdd-xiongjie-jiker/Args/src/main/java/tdd/args/CLIInputParser.java
+++ tdd-xiongjie-jiker/Args/src/main/java/tdd/args/CLIInputParser.java
@@ -1,9 +1,15 @@
 package tdd.args;
 
+import java.util.Arrays;
+
 public class CLIInputParser {
 
     public String[] parse(String argsText) {
-        return new String[0];
+        // todo 这里直接用 - 进行分割的，所以，无法支持参数值为负数的情况
+        return Arrays.stream(argsText.split("-"))
+                .map(String::trim)
+                .filter(str -> str.length() > 0)
+                .toArray(String[]::new);
     }
 
 }
